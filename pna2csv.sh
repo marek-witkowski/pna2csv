03c787e37d5f1e1314e0a18b6b7973a4e8d6b4f6
Marek Witkowski <marek.witkowski.priv@gmail.com>
2020-04-25 21:10:34 +0200

    Update pna2csv.sh

--- pna2csv.sh
+++ pna2csv.sh
@@ -2,8 +2,6 @@
 
 wget https://www.poczta-polska.pl/hermes/uploads/2013/11/spispna.pdf
 
-pdftotext -layout spispna.pdf spis.txt
-
-grep -e "^[0-9][0-9]-[0-9][0-9][0-9]"
+pdftotext -layout spispna.pdf | grep -e "^[0-9][0-9]-[0-9][0-9][0-9]" > spis.txt
 
 pnaparser.py
